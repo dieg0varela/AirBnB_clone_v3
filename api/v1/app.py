fa83d835e98e0f5208783433aa15eefe616348d4
Diego Varela <2806@holbertonschool.com>
2021-09-17 12:54:48 -0300

    Changes in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,15 +10,6 @@ from os import getenv
 
 
 app = Flask(__name__)
-"""app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True"""
-if getenv('HBNB_API_HOST') is not None:
-    HBNB_API_HOST = getenv('HBNB_API_HOST')
-else:
-    HBNB_API_HOST = '0.0.0.0'
-if getenv('HBNB_API_PORT') is not None:
-    HBNB_API_PORT = getenv('HBNB_API_PORT')
-else:
-    HBNB_API_PORT = '5000'
 
 app.register_blueprint(app_views)
 
@@ -34,4 +25,15 @@ def invalid_route(e):
 
 
 if __name__ == '__main__':
+
+    if getenv('HBNB_API_HOST') is not None:
+        HBNB_API_HOST = getenv('HBNB_API_HOST')
+    else:
+        HBNB_API_HOST = '0.0.0.0'
+
+    if getenv('HBNB_API_PORT') is not None:
+        HBNB_API_PORT = getenv('HBNB_API_PORT')
+    else:
+        HBNB_API_PORT = '5000'
+        
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
