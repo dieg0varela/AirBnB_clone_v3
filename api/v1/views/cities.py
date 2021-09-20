1ad4c9946925dc575e478f096cbbe71cabcc57a3
Diego Varela <2806@holbertonschool.com>
2021-09-20 11:07:38 -0300

    Fix error at import city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.state import State
-from models.cities import City
+from models.city import City
 import json
 
 
